43cc17b54e10f94799ce525072669bdb05dc38b8
jiadisu <71248740+JoyBoy-Su@users.noreply.github.com>
2024-04-15 16:53:04 +0800

    update

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -83,7 +83,7 @@ class DDPM(pl.LightningModule):
         self.image_size = image_size  # try conv?
         self.channels = channels
         self.use_positional_encodings = use_positional_encodings
-        self.model = DiffusionWrapper(unet_config, conditioning_key)
+        self.model = DiffusionWrapper(unet_config, conditioning_key)    # denoise model (unet), input: noise image; output: image
         count_params(self.model, verbose=True)
         self.use_ema = use_ema
         if self.use_ema:
